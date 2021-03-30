d8246cab58ec4e23ddd12dee53a07027f0bd54b5
Lucas <suporte@projetoinformatica.com.br>
2021-03-29 23:27:44 -0300

    Adição projeto Contracts (Interface)

--- PrimeiroProjeto/Contracts/Services/IPaymentService.cs
+++ PrimeiroProjeto/Contracts/Services/IPaymentService.cs
@@ -9,6 +9,6 @@ namespace PrimeiroProjeto.Contracts.Services
     {
         //A interface não possui atributos (se necessários, estes serão implementados nas classes filhas)
         //O método definido nesta interface, deverá ser sobrescrito (e não sobrecarregado) nas classes que implementam esta interface (isto garante que na definição do PaymentService (Paypal ou outro), na instanciação do objeto PaymentService, o UpCasting será feito, e o método CalculationInstallments será executado na classe filha (com suas especificidades))
-        public double CalculationInstallments(double amount, int installment);
+        public double CalculationInstallments(double amount, int installmentNumber);
     }
 }
--- PrimeiroProjeto/Contracts/Services/PaypalPaymentService.cs
+++ PrimeiroProjeto/Contracts/Services/PaypalPaymentService.cs
@@ -9,13 +9,15 @@ namespace PrimeiroProjeto.Contracts.Services
     class PaypalPaymentService : IPaymentService
     {
         //Atributos desta classe, inerentes às taxas do PayPal
+        //O 1 não é incrementado na declaração, porque primeiro deve multiplicar o coeficiente pelo installmentNumber, para depois adicionar o 1 e multiplicar o amount
         public double MonthlyInterest { get; protected set; } = 0.01;
+        //O 1 já é incrementado pelo fato de ser uma taxa final do pagamento: será ele mesmo + a taxa
         public double PaymentFee { get; protected set; } = 1.02;
 
         //Implementa o método da interface, com a forma de cálculo própria, a partir de seus valores de juros
-        public double CalculationInstallments(double amount, int installment)
+        public double CalculationInstallments(double amount, int installmentNumber)
         {
-            return (amount * (MonthlyInterest * installment + 1)) * PaymentFee;
+            return (amount * (MonthlyInterest * installmentNumber + 1)) * PaymentFee;
         }
     }
 }
